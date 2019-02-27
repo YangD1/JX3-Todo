c9c3dd4cc5f99091d0a2abbe076c02acdc1752ff
Bykaii <young@deelol.com>
2019-02-27 10:31:39 +0800

    Modified the call location of the interface. Used the store server function.

--- store/index.js
+++ store/index.js
@@ -1,11 +1,20 @@
-import axios from 'axios'
-export const state = () => ({})
+export const state = () => ({
+  user: {}
+})
 export const actions = {
-  async nuxtServerInit({ commit, duspatch }, { req }){
-    if ( req.headers.cookies ){
-      // verify
-      axios.post()
+  async nuxtServerInit({ commit, duspatch }, { req, app}){
+    try {
+      const { data } = await app.$axios.post('http://127.0.0.1:3344/verify')
+      await commit('setUser', data.user)
+    } catch (error) {
+      console.log('接口错误')
     }
   }
 }
-export const mutations = {}
+export const mutations = {
+  setUser(state, data) {
+    if (data) {
+      state.user = data
+    }
+  }
+}
