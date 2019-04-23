5a9343920890cbb0204fb8d7322dab939eb64a34
Bykaii <young@deelol.com>
2019-04-23 15:25:48 +0800

    refactor: modified left navigation drawer component style

--- components/skeleton/left-navigation-drawer.vue
+++ components/skeleton/left-navigation-drawer.vue
@@ -8,7 +8,7 @@
   <div class="text-xs-center left-nav">
     <v-alert
       :value="true"
-      color="#f1f1f1"
+      color="#fff"
     >
       <v-btn color="#4caf50" dark @click="toggleAcc = !toggleAcc">{{ toggleAcc ? '查看账号' : '添加账号' }}</v-btn>
       <v-btn @click="$emit('backLeftBaseNav')">返回</v-btn>
@@ -37,11 +37,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-.left-nav
-  position absolute
-  bottom 0
-  left 0
-  width 100%
-</style>
